87022973c00484d51674dcc0880a3f6856476e83
Lucas Gehlen <contato.lucasgehlen@gmail.com>
2023-10-03 14:00:19 -0300

    Fix URL EnvVars

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import * as dotenv from 'dotenv';
 import app from './app';
 dotenv.config();
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env._PORT, () => {
   console.log(`{ defaultUrl: http://localhost:${process.env._PORT} }`);
 });
